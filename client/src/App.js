8dbe614da4c3bbd9b4cc862d4fa7944117be7a4a
victorgalam <victorgalam2000@gmail.com>
2024-11-07 15:38:04 +0200

    07-11 client + componert + nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import AdminLogin from './components/AdminLogin';
 import AdminPanel from './components/AdminPanel';
+import UserLogin from './components/UserLogin';
+import UserRegister from './components/UserRegister'; // ייבוא קומפוננטת הרישום של המשתמש
+
 
 
 function App() {
   return (
     <Router>
       <Routes>
+
+      <Route path="/login" element={<UserLogin />} /> {/* דף התחברות */}
         {/* מסלול לדף הבית של משתמש רגיל */}
         <Route path="/" element={<Home />} />
         
@@ -18,6 +23,9 @@ function App() {
 
         {/* מסלול לפאנל ניהול אדמין (רק אם האדמין התחבר) */}
         <Route path="/admin/panel" element={<AdminPanel />} />
+
+        <Route path="/register" element={<UserRegister />} /> {/* רישום משתמש רגיל */}
+
       </Routes>
     </Router>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import Navbar from './Navbar';
 
 function Home() {
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>ברוך הבא למערכת</h1>
-      <p>זהו דף הבית למשתמש רגיל. כאן תוכל לראות את המידע האישי שלך בלבד.</p>
-      {/* הצגת המידע של המשתמש, כמו פרטים אישיים או תרגילים ספציפיים */}
+    <div>
+      <Navbar /> {/* הוספת ה-Navbar */}
+      <div style={{ textAlign: 'center', padding: '20px' }}>
+        <h1>GYM</h1>
+        <p>זהו דף הבית למשתמש רגיל. כאן תוכל לראות את המידע האישי שלך בלבד.</p>
+      </div>
     </div>
   );
 }
 
 export default Home;
- 
