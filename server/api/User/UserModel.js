23c61fef5aa97a56977977fe46cbe5b2ef0cd1db
eliya <24wowled@gmail.com>
2024-11-14 14:59:02 +0200

    "סוף יום"

--- server/api/User/UserModel.js
+++ server/api/User/UserModel.js
@@ -2,32 +2,34 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-   username: { 
-     type: String,
-     required: false,
-     trim: true,
-     maxlength: 100 },
+  username: {
+    type: String,
+    required: false,
+    trim: true,
+    maxlength: 100
+  },
 
-   email: { 
-      type: String, 
-      required: true, 
-      unique: true, 
-      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+  },
 
-   age: { type: Number, min: 0 },
+  age: { type: Number, min: 0 },
 
 
-   join: { type: Date, default: Date.now },
+  join: { type: Date, default: Date.now },
 
-   password: { 
-    type: String, 
-    required: true, 
-    minlength: 8, 
-    select: false }
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false
+  }
 
-}, 
-
-    { timestamps: true });
+},
+  { timestamps: true });
+  { timestamps: true });
 
 module.exports = mongoose.model('user', userSchema)
